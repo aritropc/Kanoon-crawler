50880f19351641bc1f6f9260a7b095f53fd40b07
aritropc <aritropc@gmail.com>
2021-11-04 20:48:17 +0530

    fetch court names

---
+++ crawler.py
@@ -0,0 +1,19 @@
+import os # for os.path.isfile()
+import requests # fetch content from websites
+import cfscrape # scrape clouflare websites
+from bs4 import BeautifulSoup as bs # for parsing html
+
+
+url_home = "https://indiankanoon.org/browse/"
+r = requests.get(url_home)
+
+scraper = cfscrape.create_scraper()
+content_home = scraper.get(url_home).content
+s = bs(content_home, features="html.parser")
+
+courts = []
+
+for court in s.findAll('a') :
+    courts.append(court.string)
+#print(s.findAll('a').string)
+print(courts)
