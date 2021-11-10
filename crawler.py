55064d238afa579302b265c1a298a46542133c4a
aritropc <aritropc@gmail.com>
2021-11-11 04:37:33 +0530

    Create dirs and save pdfs

--- crawler.py
+++ crawler.py
@@ -31,39 +31,86 @@ def crawler(url) :
 def makedir(path):
     Path(path).mkdir(parents=True, exist_ok=True)
 
-def download(url, path) :
+def download(url, path, title) :
+    print("url:", url)
+    Path(path).mkdir(parents=True, exist_ok=True)
     headers = CaseInsensitiveDict()
     headers["Content-Type"] = "application/x-www-form-urlencoded"
 
     payload = "type=pdf"
     r = scraper.post(url, data=payload, headers=headers)
 
-    filename = Path("test.pdf")
+    filename = Path(path+'/'+title+'.pdf')
     filename.write_bytes(r.content)
+
+def results(court, id, year, month, s):
+    for res in s.find_all('div', attrs={'class':'result_title'}) :
+        title = res.a.string
+        file_num = res.a['href'].split('/')[2]
+        print(f"{title} : {file_num}")
+        download(url_home+'/doc/'+file_num+'/', court+'/'+year+'/'+month+'/', title)
+
+    # print(result_url, result_id)
+
+def months(court, id, year, s) :
+    months, months_url = [], []
+    for month in s.find_all('a', href=True) :
+        months.append(month.string)
+        months_url.append(month['href'])
+
+    for i in range(3, len(months)):
+        # url = url_home+id+'/'+year+'/'+months[i]
+        url = url_home+months_url[i]
+        # print("flag1 \nurl:", url)
+        s = crawler(url)
+        # print("months: ", months_url[i])
+        results(court, id, year, months[i], s)
+
+
+
+def court_years(court, id):
+    s = crawler(url_home+id)
+    years = []
+    
+    for year in s.find_all('a', href=True) :
+        years.append(year.string)
+    # for i in range(0,3):
+    #     years.pop(0)
+    for i in range(3, len(years)):
+        url = url_home+id+'/'+years[i]
+        s = crawler(url)
+        # print("year :", years[i])
+        months(court, id, years[i], s)
+
+def courts(s) :
+    courts_name, courts_id = [], []
+    for court in s.findAll('a', href=True) :
+        courts_name.append(court.string)
+        courts_id.append(court['href'])
+        
+    # for i in range(0,3):
+    #     courts_name.pop(0)
+    #     courts_id.pop(0)
+
+    for i in range(3, len(courts_name)):
+        # print(courts_name[i], ":", courts_id[i])
+        court_years(courts_name[i], courts_id[i])
 
 def browse() :  
     print("---- Indian Kanoon web crawler ----")
 
-    courts_name = []
-    courts_url = []
-
-    s = crawler(url_browse)
-
-    for court in s.findAll('a', href=True) :
-        courts_name.append(court.string)
-        courts_url.append(court['href'])
-        # courts[court.string] = court['href']
-    for i in range(0,3):
-        courts_name.pop(0)
-        courts_url.pop(0)
+    soup = crawler(url_browse)
+    courts(soup)
         
 
-    for i in range(len(courts_name)):
+    """for i in range(len(courts_name)):
         print(f"[{i+1}] {courts_name[i]}")
 
-    court_user = int(input("\nEnter the number of the court you want to crawl : "))
-    court_url = url_home + courts_url[court_user-1]
-    print(f"\nYou have selected {courts_name[court_user-1]}.")
+    dir = Path.cwd()
+
+    # court_user = int(input("\nEnter the number of the court you want to crawl : "))
+    # court_url = url_home + courts_url[court_user-1]
+    # print(f"\nYou have selected {courts_name[court_user-1]}.")
 
     
     dir = Path.cwd()
@@ -82,7 +129,7 @@ def browse() :
     year_user = int(input("\nEnter the number of the year you want to crawl : "))
 
     print(years[year_user-1])
-    Path(dir+'/'+courts_name[court_user-1]+'/'+years[year_user]).mkdir(parents=True, exist_ok=True)
+    Path(dir+'/'+courts_name[court_user-1]+'/'+years[year_user]).mkdir(parents=True, exist_ok=True)"""
 
 
 def test():
