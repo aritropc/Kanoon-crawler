a0244699f933155ae1751b00dbac60d00898d6e8
Aritro Pal Choudhury <32550413+aritropc@users.noreply.github.com>
2021-11-11 06:03:27 +0530

    Added comments

--- crawler.py
+++ crawler.py
@@ -1,22 +1,21 @@
-# import requests # fetch content from websites
-from requests.structures import CaseInsensitiveDict
+from requests.structures import CaseInsensitiveDict # request CaseInsensitiveDicts
 import cfscrape # scrape clouflare websites
 
 from bs4 import BeautifulSoup as bs # for parsing html
 from pathlib import Path # for creating directories
 
-scraper = cfscrape.create_scraper()
-url_home = "https://indiankanoon.org"
+scraper = cfscrape.create_scraper() # create cfscrape object
+url_home = "https://indiankanoon.org" # home url
 url_browse = url_home+"/browse/"
 
-def crawler(url) :
+def crawler(url) : # scrape content from web pages
     content_home = scraper.get(url).content
     return bs(content_home, features="html.parser")
 
-def makedir(path):
+def makedir(path): # make directories if not presesnt
     Path(path).mkdir(parents=True, exist_ok=True)
 
-def download(url, path, title) :
+def download(url, path, title) : # downloads pdfs from url
     print("url:", url)
     path = 'Documents/'+path
     Path(path).mkdir(parents=True, exist_ok=True)
@@ -32,7 +31,7 @@ def download(url, path, title) :
     filename.write_bytes(r.content)
 
 
-def results(court, id, year, month, s):
+def results(court, id, year, month, s): # fetches doc links from search results
     counter = 0
     for res in s.find_all('div', attrs={'class':'result_title'}) :
         if counter < 5 :
@@ -43,7 +42,7 @@ def results(court, id, year, month, s):
             counter += 1
 
 
-def months(court, id, year, s) :
+def months(court, id, year, s) : # fetches links for each month
     months, months_url = [], []
     for month in s.find_all('a', href=True) :
         months.append(month.string)
@@ -55,7 +54,7 @@ def months(court, id, year, s) :
         results(court, id, year, months[i], s)
 
 
-def court_years(court, id):
+def court_years(court, id): # fetches links for each year
     s = crawler(url_home+id)
     years = []
     
@@ -67,7 +66,7 @@ def court_years(court, id):
         s = crawler(url)
         months(court, id, years[i], s)
 
-def courts(s) :
+def courts(s) : # fetches links for each court
     courts_name, courts_id = [], []
     for court in s.findAll('a', href=True) :
         courts_name.append(court.string)
@@ -79,7 +78,7 @@ def courts(s) :
 def main() :  
     print("---- Indian Kanoon web crawler ----")
 
-    soup = crawler(url_browse)
+    soup = crawler(url_browse) # scrape browse page for courts
     courts(soup)
 
 if __name__ == '__main__':
