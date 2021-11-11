ed93cf2887de0a12a14343cfdbb04c5852b4169d
aritropc <aritropc@gmail.com>
2021-11-11 14:22:05 +0530

    Async functions

--- crawler.py
+++ crawler.py
@@ -4,6 +4,8 @@ import cfscrape # scrape clouflare websites
 from bs4 import BeautifulSoup as bs # for parsing html
 from pathlib import Path # for creating directories
 
+import asyncio # for asynchronous process
+
 scraper = cfscrape.create_scraper() # create cfscrape object
 url_home = "https://indiankanoon.org" # home url
 url_browse = url_home+"/browse/"
@@ -15,7 +17,7 @@ def crawler(url) : # scrape content from web pages
 def makedir(path): # make directories if not presesnt
     Path(path).mkdir(parents=True, exist_ok=True)
 
-def download(url, path, title) : # downloads pdfs from url
+async def download(url, path, title) : # downloads pdfs from url
     print("url:", url)
     path = 'Documents/'+path
     Path(path).mkdir(parents=True, exist_ok=True)
@@ -31,18 +33,18 @@ def download(url, path, title) : # downloads pdfs from url
     filename.write_bytes(r.content)
 
 
-def results(court, id, year, month, s): # fetches doc links from search results
+async def results(court, id, year, month, s): # fetches doc links from search results
     counter = 0
     for res in s.find_all('div', attrs={'class':'result_title'}) :
         if counter < 5 :
             title = res.a.string
             file_num = res.a['href'].split('/')[2]
             print(f"{title} : {file_num}")
-            download(url_home+'/doc/'+file_num+'/', court+'/'+year+'/'+month+'/', title)
+            await download(url_home+'/doc/'+file_num+'/', court+'/'+year+'/'+month+'/', title)
             counter += 1
 
 
-def months(court, id, year, s) : # fetches links for each month
+async def months(court, id, year, s) : # fetches links for each month
     months, months_url = [], []
     for month in s.find_all('a', href=True) :
         months.append(month.string)
@@ -51,10 +53,10 @@ def months(court, id, year, s) : # fetches links for each month
     for i in range(3, len(months)):
         url = url_home+months_url[i]
         s = crawler(url)
-        results(court, id, year, months[i], s)
+        await results(court, id, year, months[i], s)
 
 
-def court_years(court, id): # fetches links for each year
+async def court_years(court, id): # fetches links for each year
     s = crawler(url_home+id)
     years = []
     
@@ -64,24 +66,24 @@ def court_years(court, id): # fetches links for each year
     for i in range(3, len(years)):
         url = url_home+id+'/'+years[i]
         s = crawler(url)
-        months(court, id, years[i], s)
+        await months(court, id, years[i], s)
 
-def courts(s) : # fetches links for each court
+async def courts(s) : # fetches links for each court
     courts_name, courts_id = [], []
     for court in s.findAll('a', href=True) :
         courts_name.append(court.string)
         courts_id.append(court['href'])
 
     for i in range(3, len(courts_name)):
-        court_years(courts_name[i], courts_id[i])
+        await court_years(courts_name[i], courts_id[i])
 
-def main() :  
+async def main() :  
     print("---- Indian Kanoon web crawler ----")
 
     soup = crawler(url_browse) # scrape browse page for courts
-    courts(soup)
+    await courts(soup)
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main()) # invoke function to run async
 
     
